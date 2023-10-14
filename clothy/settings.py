01c2bcbd048781e2cdf0d396395353f1edf510e3
Gilad Weinberger <giladwe21@gmail.com>
2023-10-14 20:06:10 +0300

    connect render database

--- clothy/settings.py
+++ clothy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,10 +79,7 @@ WSGI_APPLICATION = 'clothy.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	"default": dj_database_url.parse("postgres://clothy_db_user:vsVFqBuJaEXUgILrZpOq3ICsAeB0mc2n@dpg-cklch4jj89us73dt8hpg-a.oregon-postgres.render.com/clothy_db")
 }
 
 
