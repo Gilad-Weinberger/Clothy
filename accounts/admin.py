88684bf6bfdfb83d186dc875d942d9e201de0928
Gilad Weinberger <giladwe21@gmail.com>
2023-10-14 21:34:46 +0300

    adding date_joined to the customuser list_display

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import CustomUser
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name')  
+    list_display = ('email', 'first_name', 'last_name', 'date_joined')  
 
 admin.site.register(CustomUser, CustomUserAdmin)
