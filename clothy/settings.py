a60ffa54513ae0962a5992f8f9b28c7a9fd58799
Gilad Weinberger <giladwe21@gmail.com>
2023-12-12 20:27:11 +0200

    back normal

--- clothy/settings.py
+++ clothy/settings.py
@@ -103,7 +103,7 @@ STORAGES = {
         'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 
