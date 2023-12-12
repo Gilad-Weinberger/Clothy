390d3080a8ef80a1c428c241652b0b43b4cecc1d
Gilad Weinberger <giladwe21@gmail.com>
2023-12-12 17:05:20 +0200

    try to connect postgresql

--- clothy/settings.py
+++ clothy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,8 +81,12 @@ WSGI_APPLICATION = 'clothy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clothy',
+        'USER': 'clothy_user',
+        'PASSWORD': 't8SgjMsjwQQh3r82wmBdnOMovkZEuhLn',
+        'HOST': 'dpg-cleamlfgsrdc739r0180-a.oregon-postgres.render.com',
+        'PORT': '5432',
     }
 }
 
