fe8bd1ee60c7b75fa623239ecdbbd00da561cf51
Gilad Weinberger <giladwe21@gmail.com>
2023-12-12 16:37:31 +0200

    comment DATABASES

--- clothy/settings.py
+++ clothy/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'clothy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/var/task/db.sqlite3',  # Verify this path
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
 
 STORAGES = {
     'default': {
